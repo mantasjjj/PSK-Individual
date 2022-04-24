df7ce14674e73173f778a8be7db9c73e40a41f44
mantasjjj <mjakaitis18@gmail.com>
2022-04-24 21:48:17 +0300

    MyBatis implementation

---
+++ src/main/java/lt/vu/usecases/ClientsMyBatis.java
@@ -0,0 +1,41 @@
+package lt.vu.usecases;
+
+import lombok.Getter;
+import lombok.Setter;
+import lt.vu.mybatis.dao.ClientMapper;
+import lt.vu.mybatis.dao.TeamMapper;
+import lt.vu.mybatis.model.Client;
+import lt.vu.mybatis.model.Team;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Model
+public class ClientsMyBatis {
+    @Inject
+    private ClientMapper clientMapper;
+
+    @Getter
+    private List<Client> allClients;
+
+    @Getter @Setter
+    private Client clientToCreate = new Client();
+
+    @PostConstruct
+    public void init() {
+        this.loadAllClients();
+    }
+
+    private void loadAllClients() {
+        this.allClients = clientMapper.selectAll();
+    }
+
+    @Transactional
+    public String createClient() {
+        clientMapper.insert(clientToCreate);
+        return "/myBatis/clients?faces-redirect=true";
+    }
+}
--- src/main/java/lt/vu/usecases/MechanicsMyBatis.java
+++ src/main/java/lt/vu/usecases/MechanicsMyBatis.java
@@ -1,38 +1,39 @@
-//package lt.vu.usecases;
-//
-//import lombok.Getter;
-//import lombok.Setter;
-//import lt.vu.entities.Mechanic;
-//
-//import javax.annotation.PostConstruct;
-//import javax.enterprise.inject.Model;
-//import javax.inject.Inject;
-//import javax.transaction.Transactional;
-//import java.util.List;
-//
-//@Model
-//public class MechanicsMyBatis {
-//    @Inject
-//    private MechanicsMapper mechanicsMapper;
-//
-//    @Getter
-//    private List<Mechanic> allMechanics;
-//
-//    @Getter @Setter
-//    private Mechanic mechanicToCreate = new Mechanic();
-//
-//    @PostConstruct
-//    public void init() {
-//        this.loadAllMechanics();
-//    }
-//
-//    private void loadAllMechanics() {
-//        this.allMechanics = mechanicsMapper.selectAll();
-//    }
-//
-//    @Transactional
-//    public String createMechanic() {
-//        mechanicsMapper.insert(mechanicToCreate);
-//        return "/myBatis/teams?faces-redirect=true";
-//    }
-//}
+package lt.vu.usecases;
+
+import lombok.Getter;
+import lombok.Setter;
+import lt.vu.mybatis.dao.MechanicMapper;
+import lt.vu.mybatis.model.Mechanic;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Model
+public class MechanicsMyBatis {
+    @Inject
+    private MechanicMapper mechanicsMapper;
+
+    @Getter
+    private List<Mechanic> allMechanics;
+
+    @Getter @Setter
+    private Mechanic mechanicToCreate = new Mechanic();
+
+    @PostConstruct
+    public void init() {
+        this.loadAllMechanics();
+    }
+
+    private void loadAllMechanics() {
+        this.allMechanics = mechanicsMapper.selectAll();
+    }
+
+    @Transactional
+    public String createMechanic() {
+        mechanicsMapper.insert(mechanicToCreate);
+        return "/myBatis/mechanics?faces-redirect=true";
+    }
+}
